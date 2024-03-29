c69040e4f0bb2a030a76bee553c9da834a9a57d6
Krishna Ranjan <krishna.ranjan@mobiuso.com>
2024-03-29 14:11:57 +0530

    added model for chat and message

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { allUsers, authUser, registerUser } from "../controllers/user.controller.js";
+import authenticate from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").get(allUsers);
+router.route("/").get(authenticate, allUsers);
 router.route("/").post(registerUser);
 router.route("/login").post(authUser);
 
