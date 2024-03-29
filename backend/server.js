fbdc1cb25b6a5be2e52305e28156e3997b8dfe8c
Krishna Ranjan <krishna.ranjan@mobiuso.com>
2024-03-29 12:22:49 +0530

    setup connection with database

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,13 @@ import dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
 
 const app = express();
+
+app.use(express.json());
+
+connectDB()
 
 const PORT = process.env.PORT;
 
