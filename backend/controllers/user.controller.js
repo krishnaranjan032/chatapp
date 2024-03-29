69c1cbf0a76407faf9573e22cd62b7084db35aaa
Krishna Ranjan <krishna.ranjan@mobiuso.com>
2024-03-29 13:00:02 +0530

    setup user controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -3,19 +3,18 @@ import User from "../models/user.model.js";
 import generateToken from "../config/generateToken.js";
 
 export const allUsers = expressAsyncHandler(async (req, res) => {
-    const keyword = req.query.search
-      ? {
-          $or: [
-            { name: { $regex: req.query.search, $options: "i" } },
-            { email: { $regex: req.query.search, $options: "i" } },
-          ],
-        }
-      : {};
-  
-    const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
-    res.send(users);
-  });
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
 
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+});
 
 export const registerUser = expressAsyncHandler(async (req, res) => {
   // extract fields from the req/body
@@ -58,35 +57,33 @@ export const registerUser = expressAsyncHandler(async (req, res) => {
   }
 });
 
-
 export const authUser = expressAsyncHandler(async (req, res) => {
-    // get email or password from req.body
-    const { email, password } = req.body;
+  // get email or password from req.body
+  const { email, password } = req.body;
 
-    if (!email || !password) {
-        res.status(400);
-        throw new Error("Please Enter all the Feilds");
-      }
-  
-    const user = await User.findOne({ email });
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please Enter all the Feilds");
+  }
+
+  const user = await User.findOne({ email });
 
   if (!user) {
     res.status(400);
     throw new Error("User not registered");
   }
 
-  
-    if (user && (await user.matchPassword(password))) {
-      res.json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin,
-        pic: user.pic,
-        token: generateToken(user._id),
-      });
-    } else {
-      res.status(401);
-      throw new Error("Invalid Email or Password");
-    }
-  });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
+});
